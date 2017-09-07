6c1109aa398f5629e912e084142dfa9e7a2b24da
fabd <FREDtheday@Gmail.com>
2017-09-07 14:48:13 +1200

    Code cleanup:
    General Refactoring
    Added print methods so I don't vomit every time I work on this

--- src/NeuralNet.java
+++ src/NeuralNet.java
@@ -10,16 +10,19 @@ import java.util.Random;
 //TODO: Implement early stopping and regularisation
 @SuppressWarnings({"FieldCanBeLocal", "WeakerAccess", "JavaDoc"})
 public class NeuralNet {
+    private boolean testMode = true;
 
-    private static final double[][] trainingIn =  {{3, 5}, {5, 1}, {10, 2}};
-    private static final double[]   trainingOut = {75, 82, 93};
+    private final double[][] trainingIn =  {{3, 5}, {5, 1}, {10, 2}};
+    private final double[]   trainingOut = {75, 82, 93};
+
+    private final int maxIterations = 100000;
+    private final double costThreshold = 0.0000001;
 
     private int hiddenLayerNum = 1;
     private int inputLayerSize;
     private int outputLayerSize;
     private int hiddenLayerSize;
     private ActivationFunction activationFunction = new HyperbolicTangent();
-
     private ArrayList<Matrix> weights = new ArrayList<>();
     private ArrayList<Matrix> weightCostGradient = new ArrayList<>(2);
     private Matrix inputData;
@@ -32,32 +35,20 @@ public class NeuralNet {
     private Matrix dJdW1;
     private Matrix dJdW2;
     private double alpha = 0.1;
-    private int iterations = 100000;
 
     @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
     private ArrayList<Matrix> initialWeightStorage;
 
     NeuralNet(){
-        boolean testMode = true;
-        //noinspection ConstantConditions
-        if(!testMode){
-             setHyperParameters();
-         }else{
-
-             System.out.println("Running in Test mode: \n Default HyperParams set. HyperbolicTangent Activation Function used");
-
-             inputLayerSize = 2;
-             outputLayerSize = 1;
-             hiddenLayerSize = 3;
-         }
-         generateWeights();
          generateTrainingMatrices();
+         setHyperParameters();
+         generateWeights();
          train();
          System.out.println("Estimated values"); yhat.print(1,5);
          System.out.println("Training values"); targetData.print(1, 5);
     }
 
-    private void forward(){
+    private void forwardPropagate(){
         //TODO: Generalise based on hyperparams
 
         z2 = new Matrix(inputData.times(weights.get(0)).getArray());
@@ -90,33 +81,26 @@ public class NeuralNet {
     }
 
     private void train(){
+        int iterations = 1;
 
-        System.out.println("Beginning Optimisation (Modified Least Squares)\n Showing every 100th iteration:\n");
+        //TODO: This is an ugly way to avoid nullpointers
+        forwardPropagate();
+        backPropagate(yhat);
+        System.out.println("Beginning Optimisation (Modified Least Squares)\n Showing every 1000th iteration:\n");
 
-        for (int i = 0; i<iterations;i++){
-            forward();
+        while (checkEstimates()&&iterations<maxIterations){
+            forwardPropagate();
             backPropagate(yhat);
 
-            if(i%1000==0){yhat.print(1,3);}
+            if (iterations%1000 ==0){printWeights(0); printCurrentCost();}
 
-            if(computeCost(yhat)<0.0000001){System.out.println("Optimisation finished terminated by low error at " + i + " iterations: ");break;}
-
-            for(int j = 0; j<weights.size();j++){
-                weights.get(j).minusEquals(weightCostGradient.get(j).times(alpha));
-            }
-           // weights.get(0).minusEquals(dJdW1.times(alpha));
-           // weights.get(1).minusEquals(dJdW2.times(alpha));
+            update();
+            iterations++;
         }
-
-
 
         //Output
         System.out.println("\nTrained Weights: ");
-        weights.get(0).print(1,3);
-        weights.get(1).print(1, 3);
-        System.out.println("Weight Jacobian's");
-        dJdW1.print(1, 10);
-        dJdW2.print(1,10);
+        printWeights(0);
     }
 
 
@@ -124,10 +108,6 @@ public class NeuralNet {
         //TODO: Refactor this somehow, it will probably become obsolete
         inputData = normaliseMatrix(new Matrix(trainingIn));
         targetData = normaliseMatrix(new Matrix(trainingOut, 3));
-        System.out.println("Normalised Data: ");
-        inputData.print(1, 3);
-        targetData.print(1, 3);
-
     }
 
     /**
@@ -148,12 +128,12 @@ public class NeuralNet {
        //TODO: Make this actually use the hyperparams
 
        Matrix W1 = newWeightMatrix(inputLayerSize, hiddenLayerSize, inputLayerSize * hiddenLayerNum);
-       Matrix W2 = newWeightMatrix(hiddenLayerSize, outputLayerSize, hiddenLayerSize*outputLayerSize);
+       Matrix W2 = newWeightMatrix(hiddenLayerSize, outputLayerSize, hiddenLayerSize * outputLayerSize);
 
        weights.add(W1); weights.add(W2);
 
-        for (int i = 0; i < weights.size(); i++) {
-            weightCostGradient.add(new Matrix(weights.get(i).getRowDimension(), weights.get(i).getColumnDimension()));
+        for (Matrix weight : weights) {
+            weightCostGradient.add(new Matrix(weight.getRowDimension(), weight.getColumnDimension()));
         }
 
        initialWeightStorage = new ArrayList<>();
@@ -166,6 +146,12 @@ public class NeuralNet {
      * Set the HyperParameters based on console input
      */
     private void setHyperParameters(){
+        if(testMode){
+            inputLayerSize = inputData.getColumnDimension();
+            hiddenLayerSize = 2;
+            outputLayerSize = targetData.getColumnDimension();
+            return;
+        }
 
         System.out.println("Setting HyperParameters: \n Please enter the input layer size:");
         inputLayerSize = Integer.parseInt(System.console().readLine());
@@ -175,6 +161,33 @@ public class NeuralNet {
 
         System.out.println("Please enter the Hidden Layer size");
         hiddenLayerSize = Integer.parseInt(System.console().readLine());
+
+
+    }
+
+    /**
+     *
+     * @return true while the cost of the estimates is greater than the threshold value
+     */
+    public boolean checkEstimates(){
+        if(computeCost(yhat)<costThreshold){
+            System.out.println("Optimisation finished, cost in required threshold");
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * Updates every weight in the net based on the current update rule
+     */
+    private void update(){
+
+        //TODO: Implement multiple update rule options
+
+        for(int j = 0; j<weights.size();j++){
+            weights.get(j).minusEquals(weightCostGradient.get(j).times(alpha));
+        }
     }
 
 
@@ -186,7 +199,7 @@ public class NeuralNet {
     /**
      *
      * @param i Rows
-     * @param j Collums
+     * @param j Columns
      * @param m number of inputs to the next module
      * @return Weights taken from a distribution of mean = 0 and std dev = m^-1/2
      */
@@ -197,17 +210,14 @@ public class NeuralNet {
 
         for (int k = 0; k < weightLayer.getRowDimension(); k++) {
             for (int l = 0; l < weightLayer.getColumnDimension(); l++) {
+                double nextVal;
 
-                double nextval = r.nextGaussian() * 1/Math.sqrt(m);
-
-                weightLayer.set(k, l, nextval);
+                nextVal = r.nextGaussian() * 1/Math.sqrt(m);
+                weightLayer.set(k, l, nextVal);
             }
-
         }
-
     return weightLayer;
     }
-
 
     /**
      *
@@ -255,6 +265,60 @@ public class NeuralNet {
         }
         return sum;
     }
+
+    ////////////////////////////////////
+    ////////Getters/Setters/Printers////
+    ////////////////////////////////////
+
+
+
+    @SuppressWarnings("unused")
+    public void printNormalisedData(){
+        System.out.println("Normalised Data: ");
+        inputData.print(1, 3);
+        targetData.print(1, 3);
+    }
+
+    @SuppressWarnings("unused")
+    public void printTrainingOutputs() {
+        System.out.println("Training outputs:  ");
+        targetData.print(1, 3);
+    }
+
+    public void printCurrentCost(){
+        System.out.println("Cost of current estimates :");
+        System.out.println(computeCost(yhat));
+    }
+
+    @SuppressWarnings("unused")
+    public void printWeightCostGradient(int layer) {
+        assert layer<=hiddenLayerNum&&layer>-1:"Layer does not exist";
+
+        if(layer==0){
+            System.out.println("Cost Gradient of current weight layers");
+            for(Matrix m : weightCostGradient){
+                m.print(1,3);}
+        }else{
+            System.out.println("Current cost gradient of weights at layer " + layer);
+            weightCostGradient.get(layer).print(1, 3);}
+
+    }
+
+    public void printWeights(@SuppressWarnings("SameParameterValue") int layer) {
+     assert layer<=hiddenLayerNum&&layer>-1:"Layer does not exist";
+
+     if(layer==0){
+         System.out.println("Current weights");
+         for(Matrix m : weights){
+                m.print(1,3);}
+        }else{
+         System.out.println("Current weights at layer " + layer);
+         weights.get(layer).print(1, 3);}
+
+    }
+
+
+
 
     ///////////////////////////
     /////////Tests/////////////
