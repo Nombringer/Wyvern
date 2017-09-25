a6e00ee00a404bae09c9ef24049dd4ed24d3e546
fabd <FREDtheday@Gmail.com>
2017-09-25 13:13:35 +1300

    The reason why there have been no commits. All layer sizes are fully modular and Backpropagation works.

--- src/NeuralNet.java
+++ src/NeuralNet.java
@@ -28,28 +28,20 @@ public class NeuralNet {
 
     private ActivationFunction activationFunction = new HyperbolicTangent();
     private ArrayList<Matrix> weights = new ArrayList<>();
-    private ArrayList<Matrix> weightCostGradient = new ArrayList<>(2);
+    private ArrayList<Matrix> weightCostGradient = new ArrayList<>();
     private ArrayList<BiasLayer> biasLayers = new ArrayList<>();
     private Matrix inputData;
     private Matrix targetData;
 
-    private Matrix z2;
-    private Matrix z3;
     private Matrix yhat;
-    private Matrix a2;
-    private Matrix dJdW1;
-    private Matrix dJdW2;
     private double alpha = 0.1;
-
-    @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
-    private ArrayList<Matrix> initialWeightStorage;
 
     NeuralNet(){
         generateTrainingMatrices();
         if(testMode){
             inputLayerSize = inputData.getColumnDimension();
             outputLayerSize = targetData.getColumnDimension();
-            layerSizes.add(2); layerSizes.add(3); layerSizes.add(1);
+            layerSizes.add(inputLayerSize); layerSizes.add(10); layerSizes.add(3); layerSizes.add(3); layerSizes.add(outputLayerSize);
         }
          setHyperParameters();
          generateWeightLayers();
@@ -61,18 +53,7 @@ public class NeuralNet {
          System.out.println("Training values"); targetData.print(1, 5);
     }
 
-    private void forwardPropagate(Matrix input){
-        //TODO: Generalise based on hyperparams
 
-        z2 = new Matrix(input.times(weights.get(0)).getArray());
-
-        a2 = activationFunction.apply(z2);
-
-        z3 = new Matrix(a2.times(weights.get(1)).getArray());
-
-        yhat = activationFunction.apply(z3);
-
-    }
 
     private void forwardProp(Matrix input){
 
@@ -88,58 +69,41 @@ public class NeuralNet {
         yhat = biasLayers.get(biasLayers.size()-1);
     }
 
-    private void backPropagate(Matrix estimated){
-        //TODO: Generalise this based on the hyperparams
-        forwardPropagate(inputData);
 
-        //dJdW2
-        Matrix m = targetData.minus(estimated).times(-1);
-        Matrix delta3 = m.arrayTimes(activationFunction.applyGradFunc(z3));
-        dJdW2 = a2.transpose().times(delta3);
-
-        //dJdW1
-        Matrix j = delta3.times(weights.get(1).transpose());
-        Matrix delta2 = activationFunction.applyGradFunc(z2).arrayTimes(j);
-        dJdW1 = inputData.transpose().times(delta2);
-
-        weightCostGradient.set(0, dJdW1);
-        weightCostGradient.set(1, dJdW2);
-
-
-    }
 
     private void backProp(Matrix estimated){
-
+        Matrix dJdW;
         //TODO: Generalise this based on the hyperparams
         forwardProp(inputData);
 
         //dJdW2
         Matrix m = targetData.minus(estimated).times(-1);
-        Matrix delta3 = m.arrayTimes(biasLayers.get(biasLayers.size() -1).getGradients());
-        dJdW2 = biasLayers.get(biasLayers.size() -2).transpose().times(delta3);
+        Matrix delta = m.arrayTimes(biasLayers.get(biasLayers.size() -1).getGradients());
+        dJdW = biasLayers.get(biasLayers.size() -2).transpose().times(delta);
 
-        //dJdW1
-        Matrix j = delta3.times(weights.get(1).transpose());
+        weightCostGradient.set(weightCostGradient.size() -1, dJdW);
+
+
+        /*//dJdW1
+        Matrix j = delta.times(weights.get(weights.size()-1).transpose());
         Matrix delta2 = biasLayers.get(biasLayers.size() -2).arrayTimes(j);
         dJdW1 = inputData.transpose().times(delta2);
 
-        weightCostGradient.set(0, dJdW1);
-        weightCostGradient.set(1, dJdW2);
+        weightCostGradient.set(0, dJdW1); */
 
+        //Abandon all hope ye who index here.
 
-        Matrix deltaPrev = new Matrix(1, 1);
-        Matrix matrix;
+        for (int i = 1; i<weights.size(); i++){
+            Matrix matrix = delta.times(weights.get(weights.size() -i ).transpose());
+            Matrix deltaPrime = biasLayers.get(biasLayers.size() -i -1).arrayTimes(matrix);
 
-        for (int i = biasLayers.size()-1; i >=0; i--) {
-            if(i == biasLayers.size()-1){ matrix = targetData.minus(estimated).times(-1);}
-
-            else{matrix = deltaPrev.times(weights.get(i).transpose());}
-            Matrix delta = matrix.arrayTimes(biasLayers.get(i).getGradients());
-            Matrix weightLayerGradient = biasLayers.get(i-1).transpose().times(delta);
-            deltaPrev = delta;
-            weightCostGradient.set(i, weightLayerGradient);
+            if (biasLayers.size() -i -1 == 0){
+             dJdW = inputData.transpose().times(deltaPrime);}else {
+                dJdW = biasLayers.get(biasLayers.size() -i -2).transpose().times(deltaPrime);
+            }
+            weightCostGradient.set(weightCostGradient.size() -i -1, dJdW);
+            delta = deltaPrime;
         }
-
 
 
     }
@@ -158,37 +122,13 @@ public class NeuralNet {
         while (checkEstimates()&&iterations<maxIterations){
             forwardProp(inputData);
             backProp(yhat);
-            //if(iterations%10000 ==0){ printCurrentCost();}
+            if(iterations%10000 ==0){ printWeightCostGradient(0);}
             update();
             iterations++;
         }
 
     }
 
-    private void traint(){
-        int iterations = 0;
-        backPropagate(yhat);
-        System.out.println("Beginning Optimisation (Modified Least Squares)\n Showing every 1000th iteration:\n");
-        while (checkEstimates()&&iterations<maxIterations){
-            forwardPropagate(inputData);
-            backPropagate(yhat);
-            if(iterations%1000 ==0){printWeights(0); printCurrentCost();}
-            update();
-            iterations++;
-        }
-
-        //Output
-        System.out.println("\nTrained Weights: ");
-        printWeights(0);
-
-    }
-
-
-    private void generateTrainingMatrices(){
-        //TODO: Refactor this somehow, it will probably become obsolete
-        inputData = normaliseMatrix(new Matrix(trainingIn));
-        targetData = normaliseMatrix(new Matrix(trainingOut, 3));
-    }
 
     /**
      *
@@ -201,9 +141,14 @@ public class NeuralNet {
         return sum(objectiveFunction)/objectiveFunction.getColumnDimension();
     }
 
-    private void generateLayers(Matrix input){
-        layerSizes.add(input.getRowDimension());
+
+
+    private void generateTrainingMatrices(){
+        //TODO: Refactor this somehow, it will probably become obsolete
+        inputData = normaliseMatrix(new Matrix(trainingIn));
+        targetData = normaliseMatrix(new Matrix(trainingOut, 3));
     }
+
 
     /**
      * Generate the weights based on the the HyperParameters
@@ -211,18 +156,19 @@ public class NeuralNet {
     private void generateWeightLayers(){
        //TODO: Make this actually use the hyperparams
 
-       WeightLayer W1 = new WeightLayer(inputLayerSize, hiddenLayerSize, activationFunction, inputLayerSize*hiddenLayerNum);
-       WeightLayer W2 = new WeightLayer(hiddenLayerSize, outputLayerSize, activationFunction,  hiddenLayerSize*outputLayerSize);
+       //WeightLayer W1 = new WeightLayer(inputLayerSize, hiddenLayerSize, activationFunction, inputLayerSize*hiddenLayerNum);
+       //WeightLayer W2 = new WeightLayer(hiddenLayerSize, outputLayerSize, activationFunction,  hiddenLayerSize*outputLayerSize);
 
+       for(int i = 0; i<layerSizes.size() -1; i++){
+           WeightLayer weightLayer = new WeightLayer(layerSizes.get(i), layerSizes.get(i+1), activationFunction, layerSizes.get(i)*layerSizes.get(i+1));
+           weights.add(weightLayer);
+       }
 
-        weights.add(W1); weights.add(W2);
+       //weights.add(W1); weights.add(W2);
 
-        for (Matrix weight : weights) {
+       for (Matrix weight : weights) {
             weightCostGradient.add(new WeightLayer(weight.getRowDimension(), weight.getColumnDimension(), activationFunction,0 ));
-        }
-
-       initialWeightStorage = new ArrayList<>();
-       initialWeightStorage.add(W1.copy()); initialWeightStorage.add(W2.copy());
+       }
 
     }
 
@@ -385,7 +331,7 @@ public class NeuralNet {
         if(layer==0){
             System.out.println("Cost Gradient of current weight biasLayers");
             for(Matrix m : weightCostGradient){
-                m.print(1,3);}
+                m.print(1,6);}
         }else{
             System.out.println("Current cost gradient of weights at layer " + layer);
             weightCostGradient.get(layer).print(1, 3);}
