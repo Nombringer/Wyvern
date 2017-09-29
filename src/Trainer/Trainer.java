0cfdfcdfc47a674945097329b99048354b871bd5
fabd <FREDtheday@Gmail.com>
2017-09-29 14:44:30 +1300

    RailRoad tracks begin

---
+++ src/Trainer/Trainer.java
@@ -0,0 +1,14 @@
+package Trainer;
+
+import Jama.Matrix;
+import Network.NeuralNet;
+
+/**
+ * Created by fabd on 28/09/17.
+ */
+public interface Trainer {
+
+    void Train(NeuralNet net, Matrix trainingInputs, Matrix trainingOutputs);
+
+
+}
---
+++ src/Trainer/TrainerImpl.java
@@ -0,0 +1,15 @@
+package Trainer;
+
+
+/**
+ * Created by fabd on 28/09/17.
+ */
+public abstract class TrainerImpl implements Trainer {
+
+
+
+
+
+
+
+}
